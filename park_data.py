9d1d231bf69b97075e4fb57d63d628b21b5d78c6
NIJ30 <92501947+NIJ30@users.noreply.github.com>
2021-12-06 00:40:23 -0500

    Update park_data.py

--- park_data.py
+++ park_data.py
@@ -1,14 +1,48 @@
 import pandas as pd
+parks = pd.read_csv("park_data.csv")
 
-# Import csv park data
-data = pd.read_csv("park_data.csv")
+parks = parks[(parks["type"] == "Park") | (parks["type"] == "Open Space")]
+pd.set_option('display.max_columns', None)
+parks.head(210)
 
-# Filter out all non parks and open space
-data = data[(data["type"] == "Park") | (data["type"] == "Open Space")]
+parks = pd.read_csv("park_data.csv")
+parks = parks[(parks["type"] == "Park") | (parks["type"] == "Open Space")]
+count = parks["neighborhood"].value_counts()
+count
 
-# Total all parks and open spaces for each neighborhood
-neighborhood = data["neighborhood"].value_counts()
+import matplotlib.pyplot as plotter
 
-print(beechview/southside slopes)
+import numpy as np
+
+objectTypes     = ('Mount Washington', 'Hazelwood', 'Troy Hill', 'South Side Slopes', 'Beechview')
+yAxisCategories = np.arange(len(objectTypes))
+objectCount     = (5, 5, 5, 6, 6)
+stdDeviation    = (0, 0, 0, 0, 0) 
+
+figureObject, axesObject    = plotter.subplots()
+
+hBarChart                   = plotter.barh(yAxisCategories,
+
+                                           objectCount,
+
+                                           xerr=stdDeviation,
+
+                                           align='center',
+
+                                           color='red',
+
+                                           ecolor='blue')
+
+axesObject.set_title('Amount of parks/open space in Pittsburgh per city')
+
+axesObject.set_yticks(yAxisCategories)
+
+axesObject.set_yticklabels(objectTypes)
+
+axesObject.set_xlabel('Park Count')
+
+ 
+
+plotter.show()
 
 
